c0f3e675efc1e1454042a5213b3b240ec61c1441
Dharmendra6dec <dksinghpbh.somvansi@gmail.com>
2017-01-09 12:35:56 +0530

    commit for mail config

--- vatica/src/test/java/com/vaticahealth/vatica/tests/SmokeTest.java
+++ vatica/src/test/java/com/vaticahealth/vatica/tests/SmokeTest.java
@@ -4,6 +4,9 @@ import java.awt.AWTException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
@@ -13,6 +16,8 @@ import org.testng.annotations.Test;
 import com.vaticahealth.vatica.config.Configuration;
 import com.vaticahealth.vatica.pages.PHP;
 import com.vaticahealth.vatica.utils.CommonCode;
+import com.vaticahealth.vatica.utils.MailConfig;
+import com.vaticahealth.vatica.utils.MonitoringMail;
 
 public class SmokeTest extends TestAnnotation {
 
@@ -43,13 +48,13 @@ public class SmokeTest extends TestAnnotation {
 
 	}
 
-	@Test(priority = 2, description = "hra creation ")
+	@Test(priority = 3, description = "hra creation ")
 	public void smokeTest_hra_creation() throws InvocationTargetException, InterruptedException {
 		common.implictWait(20);
 		createHraTest.tc_CreateHra_1();
 	}
 
-	@Test(priority = 3, description = "hra completion ")
+	@Test(priority = 4, description = "hra completion ")
 	public void smokeTest_hra_completion() throws InvocationTargetException, InterruptedException {
 		common.implictWait(20);
 		hraTest.tc_Hra_Bi_1();
@@ -64,14 +69,14 @@ public class SmokeTest extends TestAnnotation {
 		hraTest.tc_Hra_CognitiveAssessment_1();
 	}
 
-	@Test(priority = 4, description = "T tab completion ")
+	@Test(priority = 5, description = "T tab completion ")
 	public void smokeTest_ttab_completion() throws InvocationTargetException, InterruptedException {
 		common.implictWait(20);
 		hraTest.tTabClick();
 		testTest.tc_tt_1();
 	}
 
-	@Test(priority = 5, description = "D tab completion")
+	@Test(priority = 6, description = "D tab completion")
 	public void smokeTest_dtab_completion() throws InvocationTargetException, InterruptedException {
 		common.implictWait(20);
 		hraTest.dTabClick();
@@ -80,7 +85,7 @@ public class SmokeTest extends TestAnnotation {
 
 	}
 
-	@Test(priority = 6, description = "PHP Test cases")
+	@Test(priority = 7, description = "PHP Test cases")
 	public void smokeTest_home_tcs() throws InvocationTargetException, InterruptedException, AWTException {
 		common.implictWait(20);
 		phpTest.verifyOneRowOnPhp();
@@ -88,6 +93,16 @@ public class SmokeTest extends TestAnnotation {
 		home.plainTextReport();
 		phpTest.logout();
 
+	}
+	
+	@Test(priority = 8, description = "Send email report")
+	public void QuitDriver() throws AddressException, MessagingException{
+		
+		//send mail
+		MonitoringMail mail = new MonitoringMail();
+		mail.sendMail(MailConfig.server, MailConfig.from, MailConfig.to, MailConfig.subject, MailConfig.messageBody, MailConfig.attachmentPath, MailConfig.attachmentName);
+		//driver.quit();
+		
 	}
 
 }
